fd7fb893487b743aa062c360724d530ae52c7ea0
cagatay soyer <cagataysoyer@gmail.com>
2022-01-12 23:56:35 +0300

    update: refactored connection

--- src/services/near.js
+++ src/services/near.js
@@ -1,34 +1,35 @@
-import { keyStores, Near, WalletConnection, utils } from 'near-api-js';
-// import BN from 'bn.js';
+import * as nearAPI from 'near-api-js'; // import BN from 'bn.js';
 
 export const CONTRACT_ID = 'dev-1641814602199-59406748281412';
 // const gas = new BN('70000000000000');
 
-// new NEAR is using  here to  awoid  async/await
-export const near = new Near({
+const near = new nearAPI.Near({
   networkId: 'testnet',
-  keyStore: new keyStores.BrowserLocalStorageKeyStore(),
+  keyStore: new nearAPI.keyStores.BrowserLocalStorageKeyStore(),
   nodeUrl: 'https://rpc.testnet.near.org',
   walletUrl: 'https://wallet.testnet.near.org'
 });
 
-export const wallet = new WalletConnection(near, 'guessmynumber');
+export const wallet = new nearAPI.WalletConnection(near);
+
+const contract = new nearAPI.Contract(
+  wallet.account(),
+  CONTRACT_ID,
+  // `${CONTRACT_ID}.testnet`, // the account object that is connecting
+  {
+    // name of contract you're connecting to
+    viewMethods: ['get_gameIds'], // view methods do not change state but usually return a value
+    changeMethods: ['createGame'], // change methods modify state
+    sender: wallet.account() // account object to initialize and sign transactions.
+  }
+);
 
 //function to createGame
-export const createGame = ({ attachedDeposit }) => {
-  attachedDeposit = utils.format.parseNearAmount(attachedDeposit);
-  return wallet.account().functionCall({
-    contractId: CONTRACT_ID,
-    methodName: 'createGame',
-    args: {},
-    attachedDeposit: attachedDeposit
-  });
+export const createGame = async ({ attachedDeposit }) => {
+  attachedDeposit = nearAPI.utils.format.parseNearAmount(attachedDeposit);
+  return await contract.createGame({}, 300000000000000, attachedDeposit);
 };
 
-export const getGameIds = () => {
-  return wallet.account().viewFunction({
-    contractId: CONTRACT_ID,
-    methodName: 'get_gameIds',
-    args: {}
-  });
+export const getGameIds = async () => {
+  return await contract.get_gameIds();
 };
